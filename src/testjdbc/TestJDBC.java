b14e3449e660a3b565c3dc8d471b30d233230e29
pbergqui <pbergqui@Unni>
2015-10-15 22:30:21 +0200

    Etablering

---
+++ src/testjdbc/TestJDBC.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testjdbc;
+
+/**
+ *
+ * @author pbergqui
+ */
+public class TestJDBC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MySQLAccess dao = new MySQLAccess();
+        try {
+            dao.readDataBase();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
